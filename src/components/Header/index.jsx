9cd279bf1423580e13788b6e4a51902e87b007b5
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-07-11 23:49:57 -0300

    add header css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import './header.css'
+import {Link} from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+         <Link className='logo' to='/'>Prime Flix</Link>
+         <Link className='favoritos' to='/favoritos'>Meus Filmes</Link>
+    </header>
   )
 }
