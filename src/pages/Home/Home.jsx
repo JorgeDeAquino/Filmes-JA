0d58320a0d24e24faf4c2d63e65bbd12cdb2722b
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-07-14 20:08:39 -0300

    add chamada de article para cada fime

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../services/api'
+import { Link } from 'react-router-dom'
+import './home.css'
 
 //URL DA API: /movie/now_playing?api_key=8c0bd34bf726b276d11bfb70a71ce3f0&language=pt-BR
 
@@ -17,7 +19,8 @@ export default function Home() {
         }
       })
 
-      console.log(response.data.results)
+      // console.log(response.data.results.slice(0, 10))
+      setFilmes(response.data.results.slice(0, 10))
     }
 
     loadFilmes()
@@ -26,7 +29,19 @@ export default function Home() {
 
   return (
     <div>
-        <h1>Bem vindo a home</h1>
+        <div className="container">
+          <div className="lista-filmes">
+            {filmes.map((filme) => {
+              return(
+                <article key={filme.id}>
+                  <strong>{filme.title}</strong>
+                  <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                  <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                </article>
+              )
+            })}
+          </div>
+        </div>
     </div>
   )
 }
