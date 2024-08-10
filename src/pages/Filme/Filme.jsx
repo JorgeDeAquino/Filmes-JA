bfdbc301339a7045ce1c0e02dcce990a1fe22fd1
Jorge Alexandre <jorgeaamsantos@gmail.com>
2024-08-09 22:26:25 -0300

    add id de filme e inicio de tratamento de erro

--- src/pages/Filme/Filme.jsx
+++ src/pages/Filme/Filme.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import api from '../../services/api'
 
 export default function Filme() {
+
+  const { id } = useParams()
+
+  useEffect(()=>{
+    async function loadFilme(){
+      await api.get(`/movie/${id}`, {
+        params:{
+          api_key:"8c0bd34bf726b276d11bfb70a71ce3f0",
+          language: "pt-BR",
+        }
+      })
+      .then((response)=>{
+        console.log(response)
+      })
+      .catch(()=>{
+        console.log("Filme não encontrado")
+      })
+    }
+
+    loadFilme()
+  }, [])
+
   return (
     <div>
-        <h2>Bem vindo a filme</h2>
+      <h2>Bem vindo ao filme {id}</h2>
     </div>
   )
 }
